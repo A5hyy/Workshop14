1ae6b81d1917b848d09317656bd02ea0228624ae
Ashraf <ashyalsher@gmail.com>
2022-07-26 10:12:00 +0800

    list brach

--- demo/src/main/java/vttp2022/workshop14/demo/service/ContactsRedis.java
+++ demo/src/main/java/vttp2022/workshop14/demo/service/ContactsRedis.java
@@ -4,6 +4,7 @@ package vttp2022.workshop14.demo.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import vttp2022.workshop14.demo.model.Contact;
@@ -19,13 +20,27 @@ public class ContactsRedis implements ContactsRepo {
 
     @Override
     public void save(final Contact ctc) {
-        redisTemplate.opsForValue().set(ctc.getId(), ctc);
+        redisTemplate.opsForList().rightPush(ctc.getId(), ctc.getName());
+        redisTemplate.opsForList().rightPush(ctc.getId(), ctc.getEmail());
+        redisTemplate.opsForList().rightPush(ctc.getId(), ctc.getPhoneNumber());
     }
 
     @Override
     public Contact findById(final String contactId) {
-        Contact result = (Contact) redisTemplate.opsForValue().get(contactId);
-        logger.info(">>> " + result.getEmail());
-        return result;
+        String name = (String) redisTemplate.opsForList().index(contactId, 0);
+        String email = (String) redisTemplate.opsForList().index(contactId, 1);
+        Integer phoneNumber = (Integer) redisTemplate.opsForList().index(contactId, 2);
+        logger.info(">>> name " + name);
+        logger.info(">>> email " + email);
+        logger.info(">>> phoneNumber " + phoneNumber);
+        
+        Contact ct = new Contact();
+        ct.setId(contactId);
+        ct.setName(name);
+        ct.setEmail(email);
+        ct.setPhoneNumber(phoneNumber.intValue());
+
+        return ct;
     }
-}
+
+      }
